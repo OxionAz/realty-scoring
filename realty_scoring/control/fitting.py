974ba242a6ee1a5053138120c06dc2583da74352
Alexander Zotkin <oxionaz@gmail.com>
2020-06-09 21:57:33 +0300

    requirements update + scrap/fit

--- realty_scoring/control/fitting.py
+++ realty_scoring/control/fitting.py
@@ -9,13 +9,21 @@ from sklearn.inspection import permutation_importance
 from sklearn.neural_network import MLPRegressor
 
 from realty_scoring import settings
+from realty_scoring.database.models.public_schema import Sale
 
 logger = logging.getLogger('realty_scoring')
 
 
 def load_train_data():
     try:
-        return pd.read_csv('../full_apartments_frame.csv')
+        query = Sale.select(Sale.city,
+                            Sale.district,
+                            Sale.total_area,
+                            Sale.number_of_rooms,
+                            Sale.walling,
+                            Sale.resale,
+                            Sale.price_amount)
+        return pd.DataFrame(list(query.dicts()))
     except Exception as e:
         logger.exception(str(e))
         return None
@@ -35,25 +43,22 @@ class ModelBuilder:
         logger.info('--- Building started ---')
 
         logger.info('loading train data...')
-        raw_frame = load_train_data()
+        train_frame = load_train_data()
 
         logger.info('data cleaning and validation...')
-        prepared_frame = raw_frame[
-            ['city', 'district', 'total_area', 'number_of_rooms', 'walling', 'resale', 'price_amount']
-        ]
-        prepared_frame['district'] = prepared_frame[['city', 'district']].apply(
+        train_frame['district'] = train_frame[['city', 'district']].apply(
             lambda x: settings.DISTRICTS_DICT.get(x['city'], {}).get(x['district']), axis=1)
-        prepared_frame['city'] = prepared_frame['city'].apply(settings.CITIES_DICT.get)
-        prepared_frame['number_of_rooms'] = prepared_frame['number_of_rooms'].apply(int)
-        prepared_frame['walling'] = prepared_frame['walling'].apply(settings.WALL_TYPE_DICT.get)
-        prepared_frame['resale'] = prepared_frame['resale'].apply(bool)
-        prepared_frame['price_amount'] = prepared_frame['price_amount'].apply(float)
-        prepared_frame = prepared_frame.dropna()
-        prepared_frame['district'] = prepared_frame['district'].apply(int)
+        train_frame['city'] = train_frame['city'].apply(settings.CITIES_DICT.get)
+        train_frame['number_of_rooms'] = train_frame['number_of_rooms'].apply(int)
+        train_frame['walling'] = train_frame['walling'].apply(settings.WALL_TYPE_DICT.get)
+        train_frame['resale'] = train_frame['resale'].apply(bool)
+        train_frame['price_amount'] = train_frame['price_amount'].apply(float)
+        train_frame = train_frame.dropna()
+        train_frame['district'] = train_frame['district'].apply(int)
 
         logger.info('fitting model...')
-        x = prepared_frame.drop(columns=['price_amount']).to_numpy()
-        y = prepared_frame['price_amount'].to_numpy()
+        x = train_frame.drop(columns=['price_amount']).to_numpy()
+        y = train_frame['price_amount'].to_numpy()
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01, random_state=12)
 
         deep_model = MLPRegressor(hidden_layer_sizes=(12, 12, 12, 12, 12, 6),
@@ -68,11 +73,11 @@ class ModelBuilder:
                                    random_state=0)
         for i in r.importances_mean.argsort()[::-1]:
             if r.importances_mean[i] - 2 * r.importances_std[i] > 0:
-                print(f"{prepared_frame.columns[i]:<8}"
+                print(f"{train_frame.columns[i]:<8}"
                       f"{r.importances_mean[i]:.3f}"
                       f" +/- {r.importances_std[i]:.3f}")
 
         logger.info('saving model...')
-        save_model(deep_model, 'sale_deep_.model')
+        save_model(deep_model, 'sale_deep.model')
 
         logger.info('--- Building ended ---')
--- realty_scoring/control/scraping.py
+++ realty_scoring/control/scraping.py
@@ -1,6 +1,11 @@
 import logging
 
 import pandas as pd
+import re
+import requests
+import time
+
+from lxml import etree
 
 from realty_scoring.database import db
 from realty_scoring.database.models.public_schema import Sale
@@ -8,28 +13,42 @@ from realty_scoring.database.models.public_schema import Sale
 logger = logging.getLogger('realty_scoring')
 
 
+def get_cleaned_district(address):
+    try:
+        return re.findall(r'(\w+ район),', address)[0]
+    except Exception:
+        return None
+
+
+def get_district(lat, lon):
+    result = requests.get('https://www.openstreetmap.org/geocoder/search_osm_nominatim_reverse?lat={}&latlon_digits=true&lon={}&zoom=19'.format(lat, lon))
+    tree = etree.fromstring(result.content)
+    district = tree.xpath('//a')
+    district = district[0]
+    return district.text
+
+
 class Scraper:
 
-    BASE_URL_SALE = 'https://pk.api.onliner.by/search/apartments'
-    BASE_URL_RENT = 'https://ak.api.onliner.by/search/apartments'
+    BASE_URL = 'https://pk.api.onliner.by/search/apartments'
 
-    WALL_TYPE = {
+    WALL_FILTER = {
         'panel': 'walling[]=panel',
         'brick': 'walling[]=brick',
         'monolith': 'walling[]=monolith',
         'block': 'walling[]=block'
     }
-    NUMBER_OF_ROOMS = [
+    ROOMS_FILTER = [
         'number_of_rooms[]=1',
         'number_of_rooms[]=2',
         'number_of_rooms[]=3',
         'number_of_rooms[]=4&number_of_rooms[]=5&number_of_rooms[]=6'
     ]
-    RESALE = [
+    RESALE_FILTER = [
         'resale=true',
         'resale=false'
     ]
-    CITY_BOUNDS = {
+    CITY_FILTER = {
         'Брест': 'bounds[lb][lat]=51.941725203142&bounds[lb][long]=23.492889404297&bounds[rt][lat]=52.234528294214&bounds[rt][long]=23.927536010742&v=0.34283289161620756',
         'Гродно': 'bounds[lb][lat]=53.538267122397&bounds[lb][long]=23.629531860352&bounds[rt][lat]=53.820517109806&bounds[rt][long]=24.064178466797&v=0.8871158894104579',
         'Витебск': 'bounds[lb][lat]=55.085834940707&bounds[lb][long]=29.979629516602&bounds[rt][lat]=55.357648391381&bounds[rt][long]=30.414276123047&v=0.9685201294207474',
@@ -40,11 +59,55 @@ class Scraper:
 
     @staticmethod
     def scrap():
-        try:
-            raw_frame = pd.read_csv('full_apartments_frame.csv')
-            with db.atomic():
-                Sale.truncate_table()
-                for row in raw_frame.to_dict(orient="row"):
-                    Sale.create(**row)
-        except Exception as e:
-            logger.exception(str(e))
+        logger.info('--- Scrapping started ---')
+
+        scrapped_frame = pd.DataFrame()
+
+        logger.info('requesting train data...')
+        for c, b in Scraper.CITY_FILTER.items():
+            for n in Scraper.ROOMS_FILTER:
+                for r in Scraper.RESALE_FILTER:
+                    for w, t in Scraper.WALL_FILTER.items():
+                        base_url = '{}?{}'.format(Scraper.BASE_URL, '&'.join([b, n, r, t]))
+                        result = requests.get(base_url)
+                        result = result.json()
+                        if result['apartments']:
+                            print(base_url)
+                            pages = result['page']['last'] + 1
+                            for p in range(1, pages):
+                                time.sleep(0.5)
+                                result = requests.get('{}&page={}'.format(base_url, p))
+                                query_json = result.json()
+                                for item in query_json['apartments']:
+                                    row = {'id': item['id'],
+                                           'city': c,
+                                           'address': item['location']['address'],
+                                           'latitude': item['location']['latitude'],
+                                           'longitude': item['location']['longitude'],
+                                           'kitchen_area': item['area']['kitchen'],
+                                           'living_area': item['area']['living'],
+                                           'total_area': item['area']['total'],
+                                           'walling': w,
+                                           'floor': item['floor'],
+                                           'number_of_floors': item['number_of_floors'],
+                                           'number_of_rooms': item['number_of_rooms'],
+                                           'price_amount': item['price']['amount'],
+                                           'price_currency': item['price']['currency'],
+                                           'seller': item['seller']['type'],
+                                           'resale': item['resale'],
+                                           'image': item['photo'],
+                                           'url': item['url'],
+                                           'created_at': item['created_at']}
+                                    scrapped_frame = scrapped_frame.append(row, ignore_index=True)
+
+        logger.info('enriching scrapped data...')
+        scrapped_frame['district'] = scrapped_frame.apply(lambda x: get_district(x['latitude'], x['longitude']), axis=1)
+        scrapped_frame['district'] = scrapped_frame['district'].apply(get_cleaned_district)
+
+        logger.info('saving scrapped data...')
+        with db.atomic():
+            Sale.truncate_table()
+            for row in scrapped_frame.to_dict(orient="row"):
+                Sale.create(**row)
+
+        logger.info('--- Scrapping ended ---')
